d752acad18577db1802a93f4a0e5cb22d42cbd50
Pavithra Devi <pavithradevi127@gmail.com>
2024-05-30 16:10:20 +0530

    Create tictaktoe.c

---
+++ tictaktoe.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 3
+
+char board[SIZE][SIZE];
+char currentPlayer;
+
+void initializeBoard() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            board[i][j] = ' ';
+        }
+    }
+    currentPlayer = 'X';
+}
+
+void printBoard() {
+    printf("\n");
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf(" %c ", board[i][j]);
+            if (j < SIZE - 1) printf("|");
+        }
+        printf("\n");
+        if (i < SIZE - 1) {
+            for (int j = 0; j < SIZE; j++) {
+                printf("---");
+                if (j < SIZE - 1) printf("+");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int isBoardFull() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] == ' ') {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int checkWin() {
+    // Check rows and columns
+    for (int i = 0; i < SIZE; i++) {
+        if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+            (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+            return 1;
+        }
+    }
+    // Check diagonals
+    if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+        (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+        return 1;
+    }
+    return 0;
+}
+
+void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+void makeMove() {
+    int row, col;
+    while (1) {
+        printf("Player %c, enter your move (row and column): ", currentPlayer);
+        scanf("%d %d", &row, &col);
+        if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && board[row][col] == ' ') {
+            board[row][col] = currentPlayer;
+            break;
+        } else {
+            printf("Invalid move. Try again.\n");
+        }
+    }
+}
+
+int main() {
+    initializeBoard();
+    while (1) {
+        printBoard();
+        makeMove();
+        if (checkWin()) {
+            printBoard();
+            printf("Player %c wins!\n", currentPlayer);
+            break;
+        }
+        if (isBoardFull()) {
+            printBoard();
+            printf("The game is a draw!\n");
+            break;
+        }
+        switchPlayer();
+    }
+    return 0;
+}
+
